676f2c55474f5cf182e192967d53c7ae069b9eba
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-08-16 06:13:02 +0100

    created user functionality

--- nestjs-session-based-auth1/src/app.module.ts
+++ nestjs-session-based-auth1/src/app.module.ts
@@ -2,16 +2,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './domain/user/user.module';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './database/config/typeorm.config';
-import { RedisModule } from './redis/redis.module';
+import { DBModule } from './database/db.module';
+import { UserEntity } from './domain/user/user.entity';
+// import { ConfigModule } from '@nestjs/config';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { typeOrmConfig } from './database/config/typeorm.config';
+// import { RedisModule } from './redis/redis.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal: true}),
-    TypeOrmModule.forRoot(typeOrmConfig),
-    RedisModule, // Ensure RedisModule is imported here
+    // ConfigModule.forRoot({isGlobal: true}),
+    // TypeOrmModule.forRoot(typeOrmConfig),
+    // RedisModule, // Ensure RedisModule is imported here
+    UsersModule,
+    ConfigModule.forRoot({}),
+    DBModule.forRoot({
+      entities:[UserEntity]
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ nestjs-session-based-auth1/src/domain/user/user.service.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity as User } from './user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async findByEmail(email: string): Promise<User | undefined> {
+    return this.usersRepository.findOne({ where: { email } });
+  }
+
+  async findById(id: number): Promise<User | undefined> {
+    return this.usersRepository.findOne({ where: { id: id.toString() } });
+  }
+
+  async create(user: Partial<User>): Promise<User> {
+    const newUser = this.usersRepository.create(user);
+    return this.usersRepository.save(newUser);
+  }
+}
