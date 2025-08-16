0c3590d12233d11f4d77b3451832f25146328815
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-08-16 21:44:20 +0100

    creating a new table and synching to the database

--- nestjs-cookie-based-auth2/src/domain/domain.module.ts
+++ nestjs-cookie-based-auth2/src/domain/domain.module.ts
@@ -1,10 +1,15 @@
 /* eslint-disable prettier/prettier */
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { DBModule } from "src/database/db.module";
+import { UserEntity } from "./user/entity/user.entity";
 
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal: true}),
+    DBModule.forRoot({
+      entities:[UserEntity]
+    })
   ],
   controllers: [],
   providers: [],
