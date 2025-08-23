a186f57aed3b31be8e05c6f6474627385ebbf855
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-08-23 08:23:34 +0100

    cookie based authentication

--- nestjs-cookie-based-auth2/src/domain/domain.module.ts
+++ nestjs-cookie-based-auth2/src/domain/domain.module.ts
@@ -3,13 +3,17 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { DBModule } from "src/database/db.module";
 import { UserEntity } from "./user/entity/user.entity";
+import { UserModule } from "./user/user.module";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal: true}),
     DBModule.forRoot({
       entities:[UserEntity]
-    })
+    }),
+    UserModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],
